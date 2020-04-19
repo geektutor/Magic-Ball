4a538a72515d155acfe4ebe96a6e191b69972fff
geektutor <sodiq.akinjobi@gmail.com>
2020-04-19 02:27:16 +0100

    Pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('Geek Magic Ball'),
+        centerTitle: true,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 4;
+  void onClick() {
+    setState(() {
+      ballNumber = 1 + Random().nextInt(5);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          onClick();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
